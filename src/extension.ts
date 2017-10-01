5c257c241be9d1de17454691996b336cc0f5a235
alaksej <yankouav@gmail.com>
2017-10-02 02:02:34 +0300

    wip sort all

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
+import * as vscode from 'vscode';
+import { shouldSortOnSave } from './options';
+import sortInsideEditor from './sortInsideEditor';
+import sortOnSave from './sortOnSave';
+import sortImports from './sortImports';
+
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import sortInsideEditor from './sortInsideEditor';
-import sortOnSave from './sortOnSave';
-import { shouldSortOnSave } from './options';
 
 let sortOnSaveDisposer: vscode.Disposable;
 
@@ -24,11 +26,36 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    let sortAllOnCommandDisposer = vscode.commands.registerCommand('extension.sortTypescriptImportsAll', () => {
+        // The code you place here will be executed every time your command is executed
+        const filesPromise = vscode.workspace.findFiles('src/**/*.ts');
+        // filesPromise.then(files => {
+        //     const f = files[0];
+        //     vscode.workspace.openTextDocument(f).then(d => {
+        //         vscode.window.showTextDocument(d).then(e => {
+        //             // e.show();
+        //         });
+        //     });
+        // })
+        const edit: vscode.WorkspaceEdit = new vscode.WorkspaceEdit();
+        const docs = [];
+        filesPromise.then(files => {
+            if (files && files.length) {
+                files.forEach(file => {
+                    vscode.workspace.openTextDocument(file)
+                        .then(doc => docs.push(doc)) //edit.set(doc.uri, sortImports(doc)))
+                        .then(() => vscode.workspace.applyEdit(edit));
+                });
+            }
+        });
+    });
+
     let configurationWatcher = vscode.workspace.onDidChangeConfiguration(configure);
     configure();
 
     context.subscriptions.push(
         sortOnCommandDisposer,
+        sortAllOnCommandDisposer,
         configurationWatcher);
 }
 
